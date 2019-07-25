3bc2517d58fb97ef4f5277700925becfa625a09c
rushi444 <rushi44@gmail.com>
2019-07-25 13:55:46 -0700

    working off starthere

---
+++ StartHere/src/main/java/com/lambdaschool/starthere/services/CourseServiceImpl.java
@@ -0,0 +1,48 @@
+package com.lambdaschool.school.service;
+
+import com.lambdaschool.school.exceptions.ResourceNotFoundException;
+import com.lambdaschool.school.model.Course;
+import com.lambdaschool.school.repository.CourseRepository;
+import com.lambdaschool.school.view.CountStudentsInCourses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+
+@Service(value = "courseService")
+public class CourseServiceImpl implements CourseService
+{
+    @Autowired
+    private CourseRepository courserepos;
+
+    @Override
+    public ArrayList<Course> findAll()
+    {
+        ArrayList<Course> list = new ArrayList<>();
+        courserepos.findAll().iterator().forEachRemaining(list::add);
+        return list;
+    }
+
+    @Override
+    public ArrayList<CountStudentsInCourses> getCountStudentsInCourse()
+    {
+        ArrayList<CountStudentsInCourses> count = new ArrayList<>();
+        courserepos.getCountStudentsInCourse().iterator().forEachRemaining(count::add);
+        return count;
+    }
+
+    @Transactional
+    @Override
+    public void delete(long id) throws ResourceNotFoundException
+    {
+        if (courserepos.findById(id).isPresent())
+        {
+            courserepos.deleteCourseFromStudcourses(id);
+            courserepos.deleteById(id);
+        } else
+        {
+            throw new ResourceNotFoundException(Long.toString(id));
+        }
+    }
+}
