485785a47fc4290c90318761c0970b4877f2a137
rushi444 <rushi44@gmail.com>
2019-07-25 12:53:38 -0700

    Initial Commit

---
+++ school/src/main/java/com/lambdaschool/school/SchoolApplication.java
@@ -0,0 +1,21 @@
+package com.lambdaschool.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@EnableJpaAuditing
+@SpringBootApplication
+public class SchoolApplication {
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(SchoolApplication.class, args);
+
+        DispatcherServlet dispatcherServlet = (DispatcherServlet) ctx.getBean("dispatcherServlet");
+        dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
+    }
+
+}
